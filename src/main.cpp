89f9544cf822e5d2959cfb899adbe16c77f90f38
jkmesches <jmesches@gmail.com>
2024-04-19 12:34:53 -0600

    Updated Copyright Year

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  * Course: ECE202
  * Date: Spring 2024
  *
- * Copyright (c) [Year]
+ * Copyright (c) 2024
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
